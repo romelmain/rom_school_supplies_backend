5a6dee3333e0c22d5724e4077dd7ceac5a4cc666
romelmain <romelherreratgk@gmail.com>
2024-11-18 19:57:43 -0400

    Permite que sea consumida por la pagina web

--- src/main/java/com/rom/school_supplies/Controller/UserController.java
+++ src/main/java/com/rom/school_supplies/Controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,6 +56,7 @@ public class UserController {
         }
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
 
